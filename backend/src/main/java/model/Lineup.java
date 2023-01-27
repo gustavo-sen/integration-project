cc61a15ab8a946f4c8ff6af246c88869c72310eb
gustavoabdon <gustavoabdon2@gmail.com>
2023-01-27 15:29:21 -0300

    initial commit

---
+++ backend/src/main/java/model/Lineup.java
@@ -0,0 +1,18 @@
+package model;
+
+public enum Lineup {
+    CRONOS("CRONOS"),
+    ARES(  "ARES");
+    private final String lineupName;
+
+    Lineup(String lineupName){
+        this.lineupName = lineupName;
+    }
+
+    @Override
+    public String toString() {
+        return lineupName;
+    }
+
+
+}

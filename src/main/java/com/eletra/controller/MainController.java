6edd31e4e0e72b0583fbb9d3926a80695222e3e9
gustavoabdon <gustavoabdon2@gmail.com>
2023-02-06 19:13:46 -0300

    commit

--- src/main/java/com/eletra/controller/MainController.java
+++ src/main/java/com/eletra/controller/MainController.java
@@ -1,25 +1,19 @@
 package com.eletra.controller;
 
 
-
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-
-import model.CategoriesEntity;
+import model.CategoryEntity;
 import model.LineupEntity;
-import model.ModelsEntity;
+import model.ModelEntity;
 import org.hibernate.Session;
-import sun.reflect.generics.tree.Tree;
 import util.HibernateUtil;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.function.Consumer;
 
 public class MainController implements Initializable {
 
@@ -63,18 +57,18 @@ public class MainController implements Initializable {
 
     public void treeReference(LineupEntity newValue){
 
-        List<CategoriesEntity> categoriesList = session.createQuery(String.format("FROM CategoriesEntity WHERE id_lineup = '%s'",newValue)).list();
+        List<CategoryEntity> categoriesList = session.createQuery(String.format("FROM CategoriesEntity WHERE id_lineup = '%s'",newValue)).list();
 
         //model.Lineup
         TreeItem setTreeView = new TreeItem<>(newValue);
         setTreeView.setExpanded(true);
 
         categoriesList.forEach((category)->{
-            TreeItem<CategoriesEntity> categoryItem = new TreeItem<>(category);
+            TreeItem<CategoryEntity> categoryItem = new TreeItem<>(category);
             setTreeView.getChildren().add(categoryItem);
 
             //set models
-            List<ModelsEntity> modelsList = session.createQuery(String.format("FROM ModelsEntity WHERE id_category = '%s'",category)).list();
+            List<ModelEntity> modelsList = session.createQuery(String.format("FROM ModelsEntity WHERE id_category = '%s'",category)).list();
             modelsList.forEach((model) -> categoryItem.getChildren().add(new TreeItem(category)));
 
         });

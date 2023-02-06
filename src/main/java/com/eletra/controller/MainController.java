294e5978a28a014486d3cf932b29ad8f42d340da
gustavoabdon <gustavoabdon2@gmail.com>
2023-02-06 19:17:43 -0300

    fix model name

--- src/main/java/com/eletra/controller/MainController.java
+++ src/main/java/com/eletra/controller/MainController.java
@@ -57,7 +57,7 @@ public class MainController implements Initializable {
 
     public void treeReference(LineupEntity newValue){
 
-        List<CategoryEntity> categoriesList = session.createQuery(String.format("FROM CategoriesEntity WHERE id_lineup = '%s'",newValue)).list();
+        List<CategoryEntity> categoriesList = session.createQuery(String.format("FROM CategoryEntity WHERE id_lineup = '%s'",newValue)).list();
 
         //model.Lineup
         TreeItem setTreeView = new TreeItem<>(newValue);
@@ -68,7 +68,7 @@ public class MainController implements Initializable {
             setTreeView.getChildren().add(categoryItem);
 
             //set models
-            List<ModelEntity> modelsList = session.createQuery(String.format("FROM ModelsEntity WHERE id_category = '%s'",category)).list();
+            List<ModelEntity> modelsList = session.createQuery(String.format("FROM ModelEntity WHERE id_category = '%s'",category)).list();
             modelsList.forEach((model) -> categoryItem.getChildren().add(new TreeItem(category)));
 
         });

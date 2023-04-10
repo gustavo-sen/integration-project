f62f22cae2ed35080da2ba94c000436229aee836
Gustavo Abdon <45674528+gustavoabdonsena@users.noreply.github.com>
2023-04-10 16:52:05 -0300

    commit

--- src/test/java/com/eletra/controller/MainControllerTest.java
+++ src/test/java/com/eletra/controller/MainControllerTest.java
@@ -13,6 +13,7 @@ import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.testfx.framework.junit.ApplicationTest;
 
+import javax.sound.sampled.Line;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +27,7 @@ public class MainControllerTest extends ApplicationTest {
     @Rule
     public ErrorCollector error = new ErrorCollector();
 
-    MockedStatic<GETRequest> getRequestMockedStatic = Mockito.mockStatic(GETRequest.class);
+    MockedStatic<GETRequest> getRequestMockedStatic;
 
     public MainController mc;
 
@@ -39,6 +40,8 @@ public class MainControllerTest extends ApplicationTest {
         mc.titledModels = new TitledPane();
         mc.accordion = new Accordion();
         mc.rootTreeView = new TreeItem();
+
+        getRequestMockedStatic = Mockito.mockStatic(GETRequest.class);
     }
 
     @After
@@ -52,8 +55,10 @@ public class MainControllerTest extends ApplicationTest {
     public void initializeTest01() {
         doNothing().when(mc).comboBoxSelectLineup();
         mc.initialize(null, null);
+
         error.checkThat("Checking if titledLineup is disable", mc.titledModels.isDisable(), is(true));
-        assertTrue("Check if accordion is expanded",mc.accordion.expandedPaneProperty().getValue().isExpanded());
+        assertTrue("Check if accordion is expanded"
+                ,mc.accordion.expandedPaneProperty().getValue().isExpanded());
     }
 
     @Test
@@ -79,44 +84,49 @@ public class MainControllerTest extends ApplicationTest {
 
     @Test
     public void comboBoxSelectLineupTest02(){
+
         LineupDTO[] lineupDTOS = {new LineupDTO(1,"Ares"), new LineupDTO(2,"Cronos")};
         getRequestMockedStatic.when(GETRequest::getListOfLineups).thenReturn(FXCollections.observableArrayList(lineupDTOS));
 
-        mc.comboBoxSelectLineup();
+        doNothing().when(mc).createTree(null);
 
-        assertFalse("Check if titledModels is disabled",
-                mc.titledModels.isDisabled());
+        mc.titledModels.setDisable(true); // desabilita o titledModels
+        mc.comboBoxSelectLineup();
+        mc.comboBox.setValue(new LineupDTO(1,"Ares"));
+
+        assertFalse("Check if titledModels is not disabled"
+                ,mc.titledModels.isDisabled());
     }
 
     @Test
     public void comboBoxSelectLineupTest03(){
-        LineupDTO[] lineupDTOS = {new LineupDTO(1,"Ares"), new LineupDTO(2,"Cronos")};
-        getRequestMockedStatic.when(() -> GETRequest.getListOfLineups()).thenReturn(FXCollections.observableArrayList(lineupDTOS));
-        mc.titledModels.setDisable(false);
-        mc.comboBoxSelectLineup();
-        assertFalse(mc.titledModels.isDisabled());
-    }
 
-    @Test
-    public void comboBoxSelectLineupTest04(){
+        LineupDTO lineupDTO = new LineupDTO(1,"Ares");
+
         mc.comboBoxSelectLineup();
-        mc.comboBox.setValue(new LineupDTO(1,"Ares"));
-        assertNotNull(mc.comboBox.valueProperty().getValue());
+        mc.comboBox.setValue(lineupDTO);
+
+        assertEquals("Check if comboBox receive the given set value"
+                ,mc.comboBox.valueProperty().getValue(),lineupDTO);
     }
 
     @Test
     public void createTreeTest01(){
+
         LineupDTO aresLineup = new LineupDTO(1,"Ares");
 
         CategoryDTO[] categoryDTOS = {new CategoryDTO(4,"Ares TB",aresLineup),new CategoryDTO(5,"Ares THS",aresLineup)};
         ModelDTO[] modelDTOS = {new ModelDTO(12, "Ares 7021", categoryDTOS[0]),new ModelDTO(13,"Ares 7031",categoryDTOS[1])};
+
         List<CategoryDTO> categoryDTOSList = new ArrayList<>(Arrays.asList(categoryDTOS));
         List<ModelDTO> modelDTOList = new ArrayList<>(Arrays.asList(modelDTOS));
+
         getRequestMockedStatic.when(() -> GETRequest.getListOfCategoriesFrom(aresLineup)).thenReturn(categoryDTOSList);
         getRequestMockedStatic.when(() -> GETRequest.getListOfModelsFrom(categoryDTOS[0])).thenReturn(modelDTOList);
 
         mc.createTree(aresLineup);
-        error.checkThat("Check if TreeView is expanded",mc.rootTreeView.isExpanded(),is(true));
+        error.checkThat("Check if TreeView is expanded"
+                ,mc.rootTreeView.isExpanded(),is(true));
 
     }
 
@@ -131,9 +141,19 @@ public class MainControllerTest extends ApplicationTest {
         getRequestMockedStatic.when(() -> GETRequest.getListOfCategoriesFrom(aresLineup)).thenReturn(categoryDTOSList);
         getRequestMockedStatic.when(() -> GETRequest.getListOfModelsFrom(categoryDTOS[0])).thenReturn(modelDTOList);
 
-        mc.createTree(aresLineup);
-        error.checkThat("Check if Main treeView is filled by Categories",mc.rootTreeView.getChildren().isEmpty(),is(false));
+        TreeView expectedTreeView = new TreeView();
 
+        TreeItem<CategoryDTO> categoryDTOTreeItem = (TreeItem<CategoryDTO>) new TreeItem<>(categoryDTOS);
+
+        TreeView categoryTreeView = new TreeView(categoryDTOTreeItem);
+
+        TreeItem<ModelDTO> modelDTOTreeItem = (TreeItem<ModelDTO>) new TreeItem<>(modelDTOS);
+
+
+
+        mc.createTree(aresLineup);
+
+        assertEquals("Check if Main treeView is filled by Categories",mc.rootTreeView.getChildren(),);
     }
 
     @Test
@@ -152,7 +172,8 @@ public class MainControllerTest extends ApplicationTest {
         getRequestMockedStatic.when(() -> GETRequest.getListOfModelsFrom(categoryDTOS[0])).thenReturn(modelDTOList);
 
         mc.createTree(aresLineup);
-        error.checkThat("Check if Main treeView is empty",mc.treeView.getRoot().getChildren().isEmpty(),is(false));
+        error.checkThat("Check if Main treeView is empty"
+                ,mc.treeView.getRoot().getChildren().isEmpty(),is(false));
 
     }
 
---
+++ src/test/java/com/eletra/helper/db/GETRequestTest.java
@@ -0,0 +1,7 @@
+package com.eletra.helper.db;
+
+public class GETRequestTest {
+
+
+
+}

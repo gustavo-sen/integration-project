f0a90b5626c12bb826ffb06ee09cf75db9dc14de
Gustavo Abdon <45674528+gustavoabdonsena@users.noreply.github.com>
2023-04-14 18:31:36 -0300

    Correção de testes MainControllerTest.java

--- src/test/java/com/eletra/controller/MainControllerTest.java
+++ src/test/java/com/eletra/controller/MainControllerTest.java
@@ -68,15 +68,11 @@ public class MainControllerTest extends ApplicationTest {
         mc.initialize(null, null);
 
         error.checkThat("Checking if titledLineup is disable", mc.titledModels.isDisable(), is(true));
-        assertTrue("Check if accordion is expanded"
-                ,mc.accordion.expandedPaneProperty().getValue().isExpanded());
-    }
-
-    @Test
-    public void initializeTest02() {
-        doNothing().when(mc).comboBoxSelectLineup();
-        mc.initialize(null,null);
-        verify(mc).comboBoxSelectLineup();
+        error.checkThat("Check if accordion is expanded",
+                mc.accordion.expandedPaneProperty().getValue().isExpanded(),
+                is(true)
+        );
+        verify(mc,times(1)).comboBoxSelectLineup();
     }
 
     @Test
@@ -102,58 +98,58 @@ public class MainControllerTest extends ApplicationTest {
         doNothing().when(mc).createTree(null);
 
         mc.titledModels.setDisable(true); // desabilita o titledModels
+        mc.titledModels.setExpanded(false); //modo NÃO expandido
+
         mc.comboBoxSelectLineup();
         mc.comboBox.setValue(new LineupDTO(1,"Ares"));
 
-        assertFalse("Check if titledModels is not disabled"
-                ,mc.titledModels.isDisabled());
+        error.checkThat("Check if titledModels is not disabled",
+                mc.titledModels.isDisabled(),
+                is(false)
+        );
+        error.checkThat("Check if titledModels is expanded",
+                mc.titledModels.isExpanded(),
+                is(true)
+        );
+        verify(mc,times(1)).createTree(any());
+
     }
+
 
     @Test
     public void comboBoxSelectLineupTest03(){
-
-        LineupDTO[] lineupDTOS = {new LineupDTO(1,"Ares"), new LineupDTO(2,"Cronos")};
-        lineupDTOMockedStatic.when(LineupDTOMapper::getListOfLineups).thenReturn(FXCollections.observableArrayList(lineupDTOS));
-
-        doNothing().when(mc).createTree(null);
-
-        mc.titledModels.setExpanded(false);
         mc.comboBoxSelectLineup();
         mc.comboBox.setValue(new LineupDTO(1,"Ares"));
-
-        assertTrue("Check if titledModels is expanded"
-                ,mc.titledModels.isExpanded());
-    }
-
-    @Test
-    public void comboBoxSelectLineupTest04(){
-
-        LineupDTO lineupDTO = new LineupDTO(1,"Ares");
-
-        mc.comboBoxSelectLineup();
-        mc.comboBox.setValue(lineupDTO);
-
-        assertEquals("Check if comboBox receive the given set value"
-                ,mc.comboBox.valueProperty().getValue(),lineupDTO);
+        verify(mc,times(1)).createTree(any());
     }
 
     @Test
     public void createTreeTest01(){
 
         LineupDTO aresLineup = new LineupDTO(1,"Ares");
+        LineupDTO cronosLineup = new LineupDTO(2,"Cronos");
 
-        CategoryDTO[] categoryDTOS = {new CategoryDTO(4,"Ares TB",aresLineup),new CategoryDTO(5,"Ares THS",aresLineup)};
+        LineupDTO[] lineupDTOS = {aresLineup, cronosLineup};
+        CategoryDTO[] categoryDTOS = {new CategoryDTO(4,"Ares TB",lineupDTOS[0]),new CategoryDTO(5,"Ares THS",lineupDTOS[0])};
         ModelDTO[] modelDTOS = {new ModelDTO(12, "Ares 7021", categoryDTOS[0]),new ModelDTO(13,"Ares 7031",categoryDTOS[1])};
 
-        List<CategoryDTO> categoryDTOSList = new ArrayList<>(Arrays.asList(categoryDTOS));
-        List<ModelDTO> modelDTOList = new ArrayList<>(Arrays.asList(modelDTOS));
+        lineupDTOMockedStatic.when(LineupDTOMapper::getListOfLineups).thenReturn(Arrays.asList(lineupDTOS));
+        categoryDTOMockedStatic.when(() -> CategoriesDTOMapper.getListOfCategoriesFrom(aresLineup)).thenReturn(Arrays.asList(categoryDTOS));
+        modelDTOMockedStatic.when(() -> ModelDTOMapper.getListOfModelsFrom(categoryDTOS[0])).thenReturn(Arrays.asList(modelDTOS));
 
-        categoryDTOMockedStatic.when(() -> CategoriesDTOMapper.getListOfCategoriesFrom(aresLineup)).thenReturn(categoryDTOSList);
-        modelDTOMockedStatic.when(() -> ModelDTOMapper.getListOfModelsFrom(categoryDTOS[0])).thenReturn(modelDTOList);
+        mc.rootTreeView.setExpanded(false);
+        mc.createTree(aresLineup);
+
+        List<TreeItem<CategoryDTO>> treeItem = mc.rootTreeView.getChildren();
 
         mc.createTree(aresLineup);
         error.checkThat("Check if TreeView is expanded"
                 ,mc.rootTreeView.isExpanded(),is(true));
+
+        assertEquals(
+                "Check if categories position 0 name match given"
+                ,categoryDTOS[0]
+                , treeItem.get(0).getValue());
 
     }
 
@@ -173,15 +169,37 @@ public class MainControllerTest extends ApplicationTest {
 
         mc.createTree(aresLineup);
 
-        List<TreeItem<CategoryDTO>> treeItem = FXCollections.observableArrayList(mc.rootTreeView.getChildren());
+        List<TreeItem<CategoryDTO>> treeItem = mc.rootTreeView.getChildren();
 
-        assertEquals(
-                "Check if categories position 0 name match given"
-                ,categoryDTOS[0]
-                , treeItem.get(0).getValue());
 
+        assertEquals("Check if categories position 1 name match given"
+                ,categoryDTOS[1]
+                ,treeItem.get(1).getValue());
     }
 
+    @Test
+    public void createTreeTest03(){
+
+        LineupDTO aresLineup = new LineupDTO(1,"Ares");
+        LineupDTO cronosLineup = new LineupDTO(2,"Cronos");
+
+        LineupDTO[] lineupDTOS = {aresLineup, cronosLineup};
+        CategoryDTO[] categoryDTOS = {new CategoryDTO(4,"Ares TB",lineupDTOS[0]),new CategoryDTO(5,"Ares THS",lineupDTOS[0])};
+        ModelDTO[] modelDTOS = {new ModelDTO(12, "Ares 7021", categoryDTOS[0]),new ModelDTO(13,"Ares 7031",categoryDTOS[1])};
+
+        lineupDTOMockedStatic.when(LineupDTOMapper::getListOfLineups).thenReturn(Arrays.asList(lineupDTOS));
+        categoryDTOMockedStatic.when(() -> CategoriesDTOMapper.getListOfCategoriesFrom(aresLineup)).thenReturn(Arrays.asList(categoryDTOS));
+        modelDTOMockedStatic.when(() -> ModelDTOMapper.getListOfModelsFrom(categoryDTOS[0])).thenReturn(Arrays.asList(modelDTOS));
+
+        mc.createTree(aresLineup);
+
+        List<TreeItem<CategoryDTO>> treeItem = mc.rootTreeView.getChildren();
+
+
+        assertEquals("Check if model position 0 name match given"
+                ,modelDTOS[0]
+                ,treeItem.get(0).getChildren().get(0).getValue());
+    }
     @Test
     public void createTreeTest04(){
 
@@ -198,12 +216,13 @@ public class MainControllerTest extends ApplicationTest {
 
         mc.createTree(aresLineup);
 
-        List<TreeItem<CategoryDTO>> treeItem = FXCollections.observableArrayList(mc.rootTreeView.getChildren());
+        List<TreeItem<CategoryDTO>> treeItem = mc.rootTreeView.getChildren();
 
 
-        assertEquals("Check if categories position 1 name match given"
-                ,categoryDTOS[1]
-                ,treeItem.get(1).getValue());
+        assertEquals("Check if model position 1 name match given"
+                ,modelDTOS[1]
+                ,treeItem.get(0).getChildren().get(1).getValue());
+
     }
 
     @Test
@@ -222,55 +241,7 @@ public class MainControllerTest extends ApplicationTest {
 
         mc.createTree(aresLineup);
 
-        List<TreeItem<CategoryDTO>> treeItem = FXCollections.observableArrayList(mc.rootTreeView.getChildren());
-
-
-        assertEquals("Check if model position 0 name match given"
-                ,modelDTOS[0].getName()
-                , String.valueOf(treeItem.get(0).getChildren().get(0).getValue()));
-    }
-    @Test
-    public void createTreeTest06(){
-
-        LineupDTO aresLineup = new LineupDTO(1,"Ares");
-        LineupDTO cronosLineup = new LineupDTO(2,"Cronos");
-
-        LineupDTO[] lineupDTOS = {aresLineup, cronosLineup};
-        CategoryDTO[] categoryDTOS = {new CategoryDTO(4,"Ares TB",lineupDTOS[0]),new CategoryDTO(5,"Ares THS",lineupDTOS[0])};
-        ModelDTO[] modelDTOS = {new ModelDTO(12, "Ares 7021", categoryDTOS[0]),new ModelDTO(13,"Ares 7031",categoryDTOS[1])};
-
-        lineupDTOMockedStatic.when(LineupDTOMapper::getListOfLineups).thenReturn(Arrays.asList(lineupDTOS));
-        categoryDTOMockedStatic.when(() -> CategoriesDTOMapper.getListOfCategoriesFrom(aresLineup)).thenReturn(Arrays.asList(categoryDTOS));
-        modelDTOMockedStatic.when(() -> ModelDTOMapper.getListOfModelsFrom(categoryDTOS[0])).thenReturn(Arrays.asList(modelDTOS));
-
-        mc.createTree(aresLineup);
-
-        List<TreeItem<CategoryDTO>> treeItem = FXCollections.observableArrayList(mc.rootTreeView.getChildren());
-
-
-        assertEquals("Check if model position 1 name match given"
-                ,modelDTOS[1]
-                ,treeItem.get(0).getChildren().get(1).getValue());
-
-    }
-
-    @Test
-    public void createTreeTest07(){
-
-        LineupDTO aresLineup = new LineupDTO(1,"Ares");
-        LineupDTO cronosLineup = new LineupDTO(2,"Cronos");
-
-        LineupDTO[] lineupDTOS = {aresLineup, cronosLineup};
-        CategoryDTO[] categoryDTOS = {new CategoryDTO(4,"Ares TB",lineupDTOS[0]),new CategoryDTO(5,"Ares THS",lineupDTOS[0])};
-        ModelDTO[] modelDTOS = {new ModelDTO(12, "Ares 7021", categoryDTOS[0]),new ModelDTO(13,"Ares 7031",categoryDTOS[1])};
-
-        lineupDTOMockedStatic.when(LineupDTOMapper::getListOfLineups).thenReturn(Arrays.asList(lineupDTOS));
-        categoryDTOMockedStatic.when(() -> CategoriesDTOMapper.getListOfCategoriesFrom(aresLineup)).thenReturn(Arrays.asList(categoryDTOS));
-        modelDTOMockedStatic.when(() -> ModelDTOMapper.getListOfModelsFrom(categoryDTOS[0])).thenReturn(Arrays.asList(modelDTOS));
-
-        mc.createTree(aresLineup);
-
-        List<TreeItem<CategoryDTO>> treeItem = FXCollections.observableArrayList(mc.rootTreeView.getChildren());
+        List<TreeItem<CategoryDTO>> treeItem = mc.rootTreeView.getChildren();
 
         assertEquals("Check if the title of rootTreeView is matching the Lineup chosen"
                 ,lineupDTOS[0]
